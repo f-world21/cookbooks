9d0fd372ce29663e9aef21917058de814c11b8c3
Satoshi Kobayashi <satoshi.forest@gmail.com>
2016-05-25 17:12:38 +0900

    sample debugging recipe

--- server_roles/recipes/default.rb
+++ server_roles/recipes/default.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+Chef::Log.debug("f-world21:opsworks-cookbooks/server_roles is going!")
+node[:deploy].each do |application, deploy|
+  Chef::Log.debug(application)
+  Chef::Log.debug(deploy)
+  deploy = node[:deploy][application]
+  Chef::Log.debug("after insert deploy variable")
+  Chef::Log.debug(deploy)
+end
