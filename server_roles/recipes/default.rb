19944a160a6c4cb431aba29432cfc5b38da69bf3
Satoshi Kobayashi <satoshi.forest@gmail.com>
2016-05-25 18:43:44 +0900

    test dump out yaml file

--- server_roles/recipes/default.rb
+++ server_roles/recipes/default.rb
@@ -10,10 +10,21 @@
 Chef::Log.info("f-world21:opsworks-cookbooks/server_roles is going!")
 Chef::Log.info("inspect node[:deploy] object")
 Chef::Log.info(node[:deploy])
-node[:deploy].each do |application, deploy|
+node[:deploy].each do |application, deploy_value|
   Chef::Log.info(application)
-  Chef::Log.info(deploy)
-  deploy = node[:deploy][application]
-  Chef::Log.info("after insert deploy variable")
-  Chef::Log.info(deploy)
+  Chef::Log.info(deploy_value)
+
+  Chef::Log.info("create #{deploy_value[:deploy_to]}/shared/config dir if not exist")
+  directory "#{deploy_value[:deploy_to]}/shared/config" do
+    owner "deploy"
+    group "www-data"
+    mode 0774
+    recursive true
+    action :create
+  end
+
+  Chef::Log.info("create app_data.yml")
+  file File.join(deploy_value[:deploy_to], 'shared', 'config', 'app_data.yml') do
+    content YAML.dump(deploy_value[:server_roles].to_hash)
+  end
 end
